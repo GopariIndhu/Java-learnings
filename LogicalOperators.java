8135de2c5feec8a26ea4fef7a909111cb43902d6
sushmitha <goparisushmitha@gmail.com>
2024-02-21 18:34:58 +0530

    Create LogicalOperators.java

---
+++ LogicalOperators.java
@@ -0,0 +1,27 @@
+package FirstProgram;
+
+//public class LogicalOperators {
+
+//	public static void main(String[] args) {
+//		int age = 27;
+		//age<=18
+		//age>=44
+		
+//		System.out.println(age<=42 && age>=18);
+//	}
+
+//}
+
+
+//logical OR 
+
+public class LogicalOperators {
+	public static void main(String[] args) {
+		boolean isStudent = false; 
+		boolean isLibraryMember = true;
+		
+		System.out.println(isStudent || isLibraryMember);
+	}
+}
+
+
