844c807537b759c42dccebf27ed867de33da8925
GopariIndhu <118526944+GopariIndhu@users.noreply.github.com>
2024-02-21 19:02:27 +0530

    Add files via upload

---
+++ operators.java
@@ -0,0 +1,22 @@
+package FirstProgram;
+
+public class operators1 {
+
+	public static void main(String[] args) {
+		int number1 = 33;
+		int number2 = 22;
+		//is equal to
+		System.out.println(number1 == number2);
+		//is not equal to
+		System.out.println(number1 != number2);
+		//greater than
+		System.out.println(number1 > number2);
+		//less than
+		System.out.println(number1 < number2);
+		//greater than or equal to
+		System.out.println(number1 >= number2);
+		//less than or equal to
+		System.out.println(number1 <= number2);
+	}
+
+}
