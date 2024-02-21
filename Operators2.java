a63b1a50cb8b29019b59c6a0e0243ac8ec4d9de5
GopariIndhu <118526944+GopariIndhu@users.noreply.github.com>
2024-02-21 19:04:17 +0530

    Add files via upload

---
+++ Operators2.java
@@ -0,0 +1,35 @@
+package FirstProgram;
+
+//public class Operators {
+
+	//public static void main(String[] args) {
+	//	int number1 = 10;
+	//	int number2 = 7;
+		//addition
+		//System.out.println(number1 + number2);
+		//subtraction
+		//System.out.println(number1 - number2);
+		//multiplication
+		//System.out.println(number1 * number2);
+		//division
+		//System.out.println(number1 / number2);
+		//modulus
+		//System.out.println(number1 % number2);
+//	}
+
+//}
+
+public class Operators {
+	
+	public static void main(String[] args) {
+		int number =12;
+		
+		number -=14;
+		System.out.println(number);
+	}
+}
+	
+	
+	
+	
+	
